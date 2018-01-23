9998d379fb8d4c617c26f4a75cc32bccc2629f5e
Prrrf <prrrffrrrp@gmail.com>
2018-01-23 06:48:14 +0100

    Iterator pattern. Same as: for S in SOMETHING.

--- iterator_pattern/iterator_pattern1.py
+++ iterator_pattern/iterator_pattern1.py
@@ -12,9 +12,8 @@ class CapitalIterator:
         self.index = 0
 
     def __next__(self):
-        if self.index == len(self,words):
+        if self.index == len(self.words):
             raise StopIteration()
-
 
         word = self.words[self.index]
         self.index += 1
@@ -24,3 +23,10 @@ class CapitalIterator:
         return self
 
 
+iterable = CapitalIterable('the quick brown fox jumps over the lazy dog')
+iterator = iter(iterable)
+while True:
+    try:
+        print(next(iterator))
+    except StopIteration:
+        break
